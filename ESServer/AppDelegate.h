5c42b97492ed94e1e9500f3eb7f31b434c911319
Thomas Williams <thomasforsythewilliams@gmail.com>
2014-11-05 15:56:19 -0500

    Initial Commit

---
+++ ESServer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ESServer
+//
+//  Created by Thomas Williams on 11/5/14.
+//  Copyright (c) 2014 Thomas Williams. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ESServer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ESServer
+//
+//  Created by Thomas Williams on 11/5/14.
+//  Copyright (c) 2014 Thomas Williams. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
